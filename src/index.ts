e0146ed145a05a3b008c7d0f5d0c195e1e513426
NestiaDev-id <yohanesdevano90@gmail.com>
2025-05-02 18:35:54 +0700

    install stoker and handilng notFound using js

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
 import { serve } from "@hono/node-server";
+import { OpenAPIHono } from "@hono/zod-openapi";
 import { Hono } from "hono";
+import app from "./app.js";
 
-const app = new Hono();
-
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
-
-serve({
-  fetch: app.fetch,
-  port: 3000,
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`);
-});
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
